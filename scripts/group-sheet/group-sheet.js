42937c529b6bb398ef7e2b53be76971ca677def4
Fotis Chrysoulas <f.chrysoulas@gmail.com>
2026-08-17 17:01:01 +0300

    Export MKGroupSheet from Group entry point

--- scripts/group-sheet/group-sheet.js
+++ scripts/group-sheet/group-sheet.js
@@ -1,7 +1,7 @@
 import { registerGroupSheet } from "./registration.js";
 import "./dashboard-layout.js";
 
-export { createGroupActor, SDXGroupSheet } from "./sheet.js";
+export { createGroupActor, MKGroupSheet } from "./sheet.js";
 export { registerGroupSheet };
 
 Hooks.once("init", registerGroupSheet);

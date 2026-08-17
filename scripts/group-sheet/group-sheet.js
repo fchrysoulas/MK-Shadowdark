07f73519fb55d756608b330ef433383b934b6417
Fotis Chrysoulas <f.chrysoulas@gmail.com>
2026-08-17 21:45:03 +0300

    Register unified Group time service

--- scripts/group-sheet/group-sheet.js
+++ scripts/group-sheet/group-sheet.js
@@ -1,6 +1,7 @@
 import { registerEnvironmentContextService } from "../libs/environment-context.js";
 import { registerGroupSheet } from "./registration.js";
 import { registerGroupProcedureService } from "./procedure.js";
+import { registerGroupTimeService } from "./time.js";
 import "./dashboard-behavior.js";
 
 export { createGroupActor, MKGroupSheet } from "./sheet.js";
@@ -13,8 +14,17 @@ export {
   getGroupProcedureState,
   setGroupProcedureState,
 } from "./procedure.js";
+export {
+  GROUP_TIME_ADVANCED_HOOK,
+  GROUP_TIME_RESET_HOOK,
+  getGroupTimeState,
+  getGroupElapsedTime,
+  advanceGroupTime,
+  resetGroupTime,
+} from "./time.js";
 export { registerGroupSheet };
 
 Hooks.once("init", registerGroupSheet);
 registerGroupProcedureService();
 registerEnvironmentContextService();
+registerGroupTimeService();
